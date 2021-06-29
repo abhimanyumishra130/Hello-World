5c353a3988bb77fa7aa58127c5b2782aeead04f6
dell <abhimanyumishra130@gmail.com>
2021-06-29 16:20:08 +0530

    commit!

---
+++ saved_instance/app/src/main/java/com/example/saved_instance/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.saved_instance;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTvSavedInstance;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTvSavedInstance= findViewById(R.id.mTvInstance);
+        mTvSavedInstance.setText("Welcome To School");
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("data","Screen rotated");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String str = savedInstanceState.getString("data");
+        mTvSavedInstance.setText(str);
+    }
+}
