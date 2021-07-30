067723725a7298bf114a4cd02f5e44249978d53c
dell <abhimanyumishra130@gmail.com>
2021-07-30 18:00:00 +0530

    commit!

---
+++ SendBroadcastwithintheappwithsecurityconsiderations/app/src/main/java/com/example/sendbroadcastwithintheappwithsecurityconsiderations/Recieve_broadcast.java
@@ -0,0 +1,14 @@
+package com.example.sendbroadcastwithintheappwithsecurityconsiderations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Recieve_broadcast extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recieve_broadcast);
+    }
+}
