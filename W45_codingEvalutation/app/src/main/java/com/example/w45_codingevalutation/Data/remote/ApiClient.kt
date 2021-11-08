9be6f0308bdb4216d37ba54f038de7c13ce24d1e
dell <abhimanyumishra130@gmail.com>
2021-11-08 18:15:29 +0530

    done

--- W45_codingEvalutation/app/src/main/java/com/example/w45_codingevalutation/Data/remote/ApiClient.kt
+++ W45_codingEvalutation/app/src/main/java/com/example/w45_codingevalutation/Data/remote/ApiClient.kt
@@ -14,6 +14,6 @@ interface ApiClient {
     @GET("everything?q=latest breaking news&sortBy=date&apiKey=2890a5db9eb84ba09f6880b22c992edc")
     fun getSortedData():Call<ResponseModel>
 
-    @GET("everything?q={search} news&sortBy=publishedAt&apiKey=2890a5db9eb84ba09f6880b22c992edc")
-    fun getViaSearchData(@Query("search") search:String):Call<ResponseModel>
+    @GET("everything?sortBy=publishedAt&apiKey=2890a5db9eb84ba09f6880b22c992edc")
+    fun getViaSearchData(@Query("q") search:String):Call<ResponseModel>
 }
